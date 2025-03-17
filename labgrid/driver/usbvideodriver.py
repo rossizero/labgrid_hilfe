208f149e8ec775248019cf3df13747fdc1691e9b
Sebastian Rossi <sebastian.rossi@elkiro.de>
2025-03-17 16:31:20 +0100

    more restructuring and wip

--- labgrid/driver/usbvideodriver.py
+++ labgrid/driver/usbvideodriver.py
@@ -1,11 +1,21 @@
 import subprocess
 import attr
 import numpy as np
+from enum import Enum
+import yaml
+import os
 
 from ..exceptions import InvalidConfigError
 from ..factory import target_factory
 from ..protocol import VideoProtocol
 from .common import Driver
+
+# TODO handle wrong enums
+class VideoQuality(Enum):
+    LOW = "LOW"
+    MID = "MID"
+    HIGH = "HIGH"
+    
 
 @target_factory.reg_driver
 @attr.s(eq=False)
@@ -16,6 +26,12 @@ class USBVideoDriver(Driver, VideoProtocol):
 
     def __attrs_post_init__(self):
         super().__attrs_post_init__()
+        config = os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            "usbvideodevices.yaml"
+            )
+        self.device_configs = self._load_config(config)
+
         self._prepared = False
 
         self.encoding_process = None
@@ -23,146 +39,53 @@ class USBVideoDriver(Driver, VideoProtocol):
 
         self._running = False
 
+        # TODO get width and height and channel info from config
         self.width = 1920  
         self.height = 1080
         self.channels = 4
     
+    def _load_config(self, config_path):
+        """Loads device configurations from a YAML file."""
+        try:
+            with open(config_path, "r") as file:
+                return yaml.safe_load(file)["devices"]
+        except Exception as e:
+            self.logger.error(f"Failed to load configuration: {e}")
+            return {}
+    
     def is_stream_open(self):
         return self._running
-
+    
     def get_qualities(self):
-        match = (self.video.vendor_id, self.video.model_id)
+        device_key = f"{self.video.vendor_id:04x}:{self.video.model_id:04x}"
+        device_config = self.device_configs.get(device_key, self.device_configs.get("default"))
+        qualities = {}
+        for q, cfg in device_config["qualities"].items():
+            qualities[VideoQuality[q.upper()]] = f"{device_config['format']},{cfg}"
+        return qualities
 
-    def get_qualities(self):
-        match = (self.video.vendor_id, self.video.model_id)
-        if match == (0x046d, 0x082d):
-            return ("mid", [
-                ("low", "video/x-h264,width=640,height=360,framerate=5/1"),
-                ("mid", "video/x-h264,width=1280,height=720,framerate=15/2"),
-                ("high", "video/x-h264,width=1920,height=1080,framerate=10/1"),
-                ])
-        elif match == (0x046d, 0x0892):
-            return ("mid", [
-                ("low", "image/jpeg,width=640,height=360,framerate=5/1"),
-                ("mid", "image/jpeg,width=1280,height=720,framerate=15/2"),
-                ("high", "image/jpeg,width=1920,height=1080,framerate=10/1"),
-                ])
-        elif match == (0x046d, 0x08e5): # Logitech HD Pro Webcam C920
-            return ("mid", [
-                ("low", "image/jpeg,width=640,height=360,framerate=5/1"),
-                ("mid", "image/jpeg,width=1280,height=720,framerate=15/2"),
-                ("high", "image/jpeg,width=1920,height=1080,framerate=10/1"),
-                ])
-        elif match == (0x1224, 0x2825): # LogiLink UA0371
-            return ("mid", [
-                ("low", "image/jpeg,width=640,height=480,framerate=30/1"),
-                ("mid", "image/jpeg,width=1280,height=720,framerate=30/1"),
-                ("high", "image/jpeg,width=1920,height=1080,framerate=30/1"),
-                ])
-        elif match == (0x32a8, 0x338b): # also LogiLink UA0371
-            return ("mid", [
-                ("low", "image/jpeg,width=640,height=480,framerate=30/1"),
-                ("mid", "image/jpeg,width=1280,height=720,framerate=30/1"),
-                ("high", "image/jpeg,width=1920,height=1080,framerate=30/1"),
-                ])
-        elif match == (0x05a3, 0x9331): # WansView Webcam 102
-            return ("mid", [
-                ("low","video/x-h264,width=640,height=360,framerate=30/1"),
-                ("mid","video/x-h264,width=1280,height=720,framerate=30/1"),
-                ("high","video/x-h264,width=1920,height=1080,framerate=30/1"),
-                ])
-        elif match == (0x534d, 0x2109): # MacroSilicon
-            return ("mid", [
-                ("low", "image/jpeg,width=720,height=480,framerate=10/1"),
-                ("mid", "image/jpeg,width=1280,height=720,framerate=10/1"),
-                ("high", "image/jpeg,width=1920,height=1080,framerate=10/1"),
-                ])
-        elif match == (0x1d6c, 0x0103): # HD 2MP WEBCAM
-            return ("mid", [
-                ("low", "video/x-h264,width=640,height=480,framerate=25/1"),
-                ("mid", "video/x-h264,width=1280,height=720,framerate=25/1"),
-                ("high", "video/x-h264,width=1920,height=1080,framerate=25/1"),
-                ])
-        elif match == (0x0c45, 0x636b): # LogiLink UA0379 / Microdia
-            return ("mid", [
-                ("low", "image/jpeg,width=640,height=480,pixel-aspect-ratio=1/1,framerate=30/1"),
-                ("mid", "image/jpeg,width=1280,height=720,pixel-aspect-ratio=1/1,framerate=30/1"),
-                ("high", "image/jpeg,width=1920,height=1080,pixel-aspect-ratio=1/1,framerate=30/1"),
-                ])
-        elif match == (0x0c45, 0x636d): # AUKEY PC-LM1E
-            return ("mid", [
-                ("low", "image/jpeg,width=640,height=480,pixel-aspect-ratio=1/1,framerate=30/1"),
-                ("mid", "image/jpeg,width=864,height=480,pixel-aspect-ratio=1/1,framerate=30/1"),
-                ("high", "image/jpeg,width=1280,height=1024,pixel-aspect-ratio=1/1,framerate=30/1"),
-                ])
-        elif match ==(0x046d, 0x0826): # Logitech C525 oder so
-            return ("mid", [
-                ("low", "image/jpeg,width=640,height=480,framerate=30/1"),
-                ("mid", "image/jpeg,width=1280,height=720,framerate=30/1"),
-                ("high", "image/jpeg,width=1920,height=1080,framerate=30/1"),
-                ])
-        self.logger.warning(
-            "Unkown USB video device {:04x}:{:04x}, using fallback pipeline."
-            .format(*match))
-        return ("mid", [
-            ("low", "image/jpeg,width=640,height=480,framerate=30/1"),
-            ("mid", "image/jpeg,width=1280,height=720,framerate=30/1"),
-            ("high", "image/jpeg,width=1920,height=1080,framerate=30/1"),
-            ])
+    def get_pipeline(self, quality:VideoQuality=VideoQuality.MID, controls:str=None):
+        device_key = f"{self.video.vendor_id:04x}:{self.video.model_id:04x}"
+        device_config =  self.device_configs.get(device_key)
 
-    def select_caps(self, hint=None):
-        default, variants = self.get_qualities()
-        variant = hint if hint else default
-        for name, caps in variants:
-            if name == variant:
-                return caps
-        raise InvalidConfigError(
-            f"Unknown video format {variant} for device {self.video.vendor_id:04x}:{self.video.model_id:04x}"  # pylint: disable=line-too-long
-        )
+        caps = self.get_qualities().get(quality)
+        controls = controls or device_config.get("controls")
+        inner = device_config.get("inner")
 
-    def get_pipeline(self, path, caps, controls=None):
-        match = (self.video.vendor_id, self.video.model_id)
-        if match == (0x046d, 0x082d):
-            controls = controls or "focus_auto=1"
-            inner = "h264parse"
-        elif match == (0x046d, 0x0892):
-            controls = controls or "focus_auto=1"
-            inner = None
-        elif match == (0x046d, 0x08e5):
-            controls = controls or "focus_auto=1"
-            inner = None
-        elif match == (0x1224, 0x2825): # LogiLink UA0371
-            inner = None  # just forward the jpeg frames
-        elif match == (0x05a3, 0x9331): # WansView Webcam 102
-            inner = "h264parse"
-        elif match == (0x534d, 0x2109):
-            inner = None  # just forward the jpeg frames
-        elif match == (0x1d6c, 0x0103):
-            controls = controls or "focus_auto=1"
-            inner = "h264parse"
-        elif match == (0x0c54, 0x636b):
-            controls = controls or "focus_auto=1"
-            inner = None  # just forward the jpeg frames
-        elif match == (0x0c54, 0x636d):
-            controls = controls or "focus_auto=1"
-            inner = None  # just forward the jpeg frames
-        else: # fallback pipeline
-            inner = None  # just forward the jpeg frames
-
-        pipeline = f"v4l2src device={path} "
+        pipeline = f"v4l2src device={self.video.path} "
         if controls:
             pipeline += f"extra-controls=c,{controls} "
         pipeline += f"! {caps} "
         if inner:
             pipeline += f"! {inner} "
+        pipeline += "! matroskamux streamable=true ! fdsink"
 
-        pipeline += "! matroskamux streamable=true ! fdsink"        
         return pipeline
 
+
     @Driver.check_active
-    def stream(self, caps_hint=None, controls=None):
-        caps = self.select_caps(caps_hint)
-        pipeline = self.get_pipeline(self.video.path, caps, controls)
+    def stream(self, caps_hint:str="low", controls=None):
+        pipeline = self.get_pipeline(VideoQuality[caps_hint.upper()], controls)
         
         tx_cmd = self.video.command_prefix + ["gst-launch-1.0", "-q"]
         tx_cmd += pipeline.split()
@@ -200,9 +123,8 @@ class USBVideoDriver(Driver, VideoProtocol):
         tx.communicate()
     
     @Driver.check_active
-    def start_stream(self, caps_hint=None, controls=None):
-        caps = self.select_caps(caps_hint)
-        pipeline = self.get_pipeline(self.video.path, caps, controls)
+    def start_stream(self, caps_hint:str="mid", controls=None):
+        pipeline = self.get_pipeline(VideoQuality[caps_hint.upper()], controls)
         tx_cmd = self.video.command_prefix + ["gst-launch-1.0", "-q"]
         tx_cmd += pipeline.split()
 
